f5daa41bd02fdd013421996c7b922ad4e62cde80
Dominic <johndominicjasmin@gmail.com>
2021-05-19 09:07:41 +0800

    fixing constructor bug at imagefilemanager

--- BrgySystem/BrgySystem/Classes/checkInputValidity.vb
+++ BrgySystem/BrgySystem/Classes/checkInputValidity.vb
@@ -1,21 +1,27 @@
 ﻿Imports Bunifu.UI.WinForms
 
 Module checkInputValidity
-    Function ContainsSpecialChars(value As String) As Boolean
+    Function InputContainsSpecialCharacter(value As String) As Boolean
         Return value.IndexOfAny("[~`!@#$%^&*()-+=|{}':;.,<>/?]".ToCharArray) <> -1
     End Function
     Function InputIsNull(valueInput As String) As Boolean
-        Return valueInput.Length = 0
+        Return valueInput.Trim.Length = 0
 
     End Function
     Sub setCharacterLengthLimit(length As Integer, value As String)
         'to be use somewhere else
     End Sub
 
-    Function isDateValidIn(date_ As BunifuDatePicker) As Boolean
+    Function isDateOrBirthdayInvalid(date_ As BunifuDatePicker) As Boolean
         Return date_.Value.Year >= Date.Now.Year
     End Function
 
+    Function InputContainsNumber(value As String) As Boolean
+        Return value.IndexOfAny("123456789".ToCharArray) <> -1
+    End Function
 
 
+    Function InputContainsLetter(value As String) As Boolean
+        Return value.IndexOfAny("ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray) <> -1
+    End Function
 End Module
